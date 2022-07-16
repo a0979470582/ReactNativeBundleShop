85672b4c2bef02448940d8f419ed805f1bfd3769
a0979470582 <a0979470582@gmail.com>
2022-07-16 14:58:08 +0800

    feat(rn): implement two buttons of main page 先讓主頁有一個標題和兩個按鈕，以及移除掉主頁初始的程式碼。

--- App.js
+++ App.js
@@ -1,112 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import { Color } from './Color';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export function App() {
+  const initBusiness1 = () => {
+    console.log('press button init initBusiness1....');
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const initBusiness2 = () => {
+    console.log('press button init initBusiness2....');
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  return <SafeAreaView style={styles.safeAreaView}>
+    <StatusBar style={{ backgroundColor: Color.background }} barStyle={'light-content'} />
+    <Text style={styles.titleText}>Bundle Shop</Text>
+    <TouchableOpacity style={styles.button} onPress={initBusiness1}>
+      <Text style={styles.buttonText}>Business1</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={initBusiness2}>
+      <Text style={styles.buttonText}>Business2</Text>
+    </TouchableOpacity>
+  </SafeAreaView>
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: Color.whitePure,
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  titleText: {
+    fontSize: 55,
+  }, buttonText: {
+    fontSize: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: Color.whitePure,
+    borderWidth: 1,
+    marginTop: 16,
+    padding: 16,
   },
 });
-
-export default App;
---
+++ Color.ts
@@ -0,0 +1,4 @@
+export class Color {
+  static background = '#181818';
+  static whitePure = '#ffffff';
+}
